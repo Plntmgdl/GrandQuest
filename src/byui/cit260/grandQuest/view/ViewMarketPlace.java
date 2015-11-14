27419b1e9df9a1a37d8733ddbf48e31c21dc051c
Devin Irvine <devin.irvine@gmail.com>
2015-11-14 00:40:24 -0700

    11/13/2015 Added ViewMarketPlace

---
+++ src/byui/cit260/grandQuest/view/ViewMarketPlace.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.grandQuest.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author devin_000
+ */
+public abstract class ViewMarketPlace implements ViewInterface {
+    
+    private String promptMessage;
+    
+    public ViewMarketPlace(String promptMessage) {
+        this.promptMessage = promptMessage;
+    }
+
+    public String getPromptMessage() {
+        return promptMessage;
+    }
+
+    public void setPromptMessage(String message) {
+        this.promptMessage = message;
+    }
+    
+    public void display() {
+        String value = "";
+        boolean done = false;
+        
+        do {
+            System.out.println(this.promptMessage);
+            value = this.getInput();
+            done = this.doAction(value);
+        } while (!done);
+    }
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        String value = null;
+        
+        while (!valid) {
+            
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+            if (value.length()<1) {
+                System.out.println("You mst enter a value.");
+                continue;
+            }
+            
+            break;
+            }
+        
+        return value;
+    }
+
+    private boolean doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+        
