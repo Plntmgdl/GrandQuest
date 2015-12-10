ac985acd6beaefba4e075c9a0a04396f03da6b42
Jassen <Jassen@BigDaddy>
2015-12-09 20:33:11 -0700

    Merge origin/master
    Conflicts:
    	src/GrandQuest/GrandQuest.java
    	src/byui/cit260/grandQuest/view/GameMenuView.java
    	src/byui/cit260/grandQuest/view/MainMenuView.java
    	src/byui/cit260/grandQuest/view/StartProgramView.java
    	src/byui/cit260/grandQuest/view/View.java

--- src/byui/cit260/grandQuest/view/GameMenuView.java
+++ src/byui/cit260/grandQuest/view/GameMenuView.java
@@ -25,83 +25,12 @@ public class GameMenuView extends View{
                 + "\nI - View Inventory"
                 + "\nC - View Characters"
                 + "\nW - View Wagon Status"
-                + "\nM - Return to main menu"
+                + "\nE - Return to previous menu"
                 + "\n--------------------------------------");
     }
     
     
-   
- 
-    public boolean doAction(char selection) {
-        
-        switch (selection) {
-            case 'V': // Travel to new location
-                this.displayMap();
-                break;
-            case 'I': // View list of items in inventory
-                GameMenuView.viewInventory();
-                break;
-            case 'C': // View list of actors
-                this.viewCharacters();
-                break;
-            case 'W': // View the wagon's status
-                this.viewWagonStatus();
-                break;
-            case 'M':
-                return true;
-            default:
-                System.out.println("Invalid selection");
-                break;
-            // ...
-        }
-        return false;
-    }
-    
-    // BroJones Begin
-    private void displayMap() {
-        Map map = GrandQuest.GrandQuest.getGame().getMap();
-    
-        Location [][] locations = map.getLocations();
-        
-        for(int x = 0; x < map.getRowCount(); x++){
-            for(int y = 0; y < map.getColumnCount(); y++)
-            {
-                if (locations[x][y].getScene().getMapSymbol()=="")
-                    System.out.print("|    ");
-                else 
-                    System.out.print("| "+locations[x][y].getScene().getMapSymbol()+" ");
-            }
-            System.out.println("|");
-        }
-           
-    }
-    
-    private void viewCharacters(){
-        
-    }
-    
-    private void viewWagonStatus(){
-        
-    }
- public static void viewInventory() {
-        // get the sorted list of inventory items for the current game
-        InventoryItem[] inventory = GameControl.getSortedInventoryList();
-        
-        System.out.println("\nList of Inventory Items");
-        System.out.println("Description" + "\t" +
-                           "Required" + "\t" +
-                           "In Stock");
-        
-        // for each inventory item
-        for (InventoryItem inventoryItem : inventory) {
-            // DISPLAY the description, the required amount and amount in stock
-            System.out.println(inventoryItem.getDescription() + "\t    " +
-                               inventoryItem.getRequiredAmount() + "\t   " +
-                               inventoryItem.getQuantityInStock());
-        }
-    }
-
-    @Override
+   @Override
     public boolean doAction(Object obj) {
         String value=(String)obj;
         value=value.toUpperCase();
@@ -128,6 +57,55 @@ public class GameMenuView extends View{
    
     }
 
+ 
+//   
+    
+    // BroJones Begin
+    private void displayMap() {
+        Map map = GrandQuest.GrandQuest.getGame().getMap();
+            System.out.println("\nThe Land");
+            
+        Location [][] locations = map.getLocations();
+        
+        for(int x = 0; x < map.getRowCount(); x++){
+            for(int y = 0; y < map.getColumnCount(); y++)
+            {
+                if (locations[x][y].getScene().getMapSymbol()=="")
+                    System.out.print("|    ");
+                else 
+                    System.out.print("| "+locations[x][y].getScene().getMapSymbol()+" ");
+            }
+            System.out.println("|");
+        }
+           
+    }
+    
+    
+ public static void viewInventory() {
+        // get the sorted list of inventory items for the current game
+        InventoryItem[] inventory = GameControl.getSortedInventoryList();
+        
+        System.out.println("\nList of Inventory Items");
+        System.out.println("Description" + "\t" +
+                           "Required" + "\t" +
+                           "In Stock");
+        
+        // for each inventory item
+        for (InventoryItem inventoryItem : inventory) {
+            // DISPLAY the description, the required amount and amount in stock
+            System.out.println(inventoryItem.getDescription() + "\t    " +
+                               inventoryItem.getRequiredAmount() + "\t   " +
+                               inventoryItem.getQuantityInStock());
+        }
+    }
+
+    private void viewCharacters(){
+        
+    }
+    
+    private void viewWagonStatus(){
+        
+    }
 //    @Override
 //    public void displayMenu() {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
