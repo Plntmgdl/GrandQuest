0a5f27ab4a7e0432bec1940587fb15752eccaea3
Jassen <Jassen@BigDaddy>
2015-12-09 20:21:43 -0700

    Merge origin/master
    Conflicts:
    	src/GrandQuest/GrandQuest.java
    	src/byui/cit260/grandQuest/view/GameMenuView.java
    	src/byui/cit260/grandQuest/view/MainMenuView.java
    	src/byui/cit260/grandQuest/view/StartProgramView.java
    	src/byui/cit260/grandQuest/view/View.java

--- src/byui/cit260/grandQuest/control/MapControl.java
+++ src/byui/cit260/grandQuest/control/MapControl.java
@@ -32,4 +32,4 @@ public class MapControl {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 //    
-}
+
