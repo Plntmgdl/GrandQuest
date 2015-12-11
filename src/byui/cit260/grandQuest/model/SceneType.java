f6c7f468c5c61ce8865b436037be7c8e4ba22228
Jassen <Jassen@BigDaddy>
2015-12-10 21:59:06 -0700

    Merge origin/master
    Conflicts:
    	src/GrandQuest/GrandQuest.java
    	src/byui/cit260/grandQuest/view/GameMenuView.java
    	src/byui/cit260/grandQuest/view/MainMenuView.java
    	src/byui/cit260/grandQuest/view/StartProgramView.java
    	src/byui/cit260/grandQuest/view/View.java

--- src/byui/cit260/grandQuest/model/SceneType.java
+++ src/byui/cit260/grandQuest/model/SceneType.java
@@ -13,30 +13,30 @@ import java.io.Serializable;
  * @author devin_000
  */
 public enum SceneType implements Serializable{
-    start(""),
-    village(""),
-    marketplace(""),
-    tavern(""),
-    inn(""),
-    bathtub(""),
-    road(""),
-    bridge(""),
-    forest(""),
-    finish("");
+    start,
+    village,
+    marketplace,
+    tavern,
+    inn,
+    bathtub,
+    road,
+    bridge,
+    forest,
+    finish;
 
-    private final String description;
-    private final Point coordinates;
-    
-    SceneType(String description) {
-        this.description = description;
-        coordinates = new Point(1,1);
-    }
-    
-    public String getDescription() {
-        return description;
-    }    
-    public Point getCoordinates() {
-        return coordinates;
-    }
+//    private final String description;
+//    private final Point coordinates;
+//    
+//    SceneType(String description) {
+//        this.description = description;
+//        coordinates = new Point(1,1);
+//    }
+//    
+//    public String getDescription() {
+//        return description;
+//    }    
+//    public Point getCoordinates() {
+//        return coordinates;
+//    }
 }
   
