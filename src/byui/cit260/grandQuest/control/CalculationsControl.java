065c2557f3ba584829d9778e6d57c6ce3d35b610
devin_000 <devin_000@IrvineFamily-HP.T-mobile.com>
2015-12-14 21:21:29 -0700

    Adding in calculations and BathTubView

--- src/byui/cit260/grandQuest/control/CalculationsControl.java
+++ src/byui/cit260/grandQuest/control/CalculationsControl.java
@@ -37,6 +37,10 @@ public class CalculationsControl {
  
     }
 
+    public static double CalculateVolumeOfTub() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
    
     
     
